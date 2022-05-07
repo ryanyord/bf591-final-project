aec6e457d22f6ab130bdfee4cc76755539dd2f96
ryanyord <ryan.yordanoff@outlook.com>
2022-05-06 23:56:48 -0400

    better structure

--- app.R
+++ app.R
@@ -13,17 +13,127 @@ ui <- fluidPage(
   
   mainPanel(
     tabsetPanel(
-#---------------------------Tab 1: Samples------------------------------------
+      #---------------------------Tab 1: Samples------------------------------------
       # Tab 1: Samples
       tabPanel("Samples",
+               
+               # Sidebar layout
+               sidebarLayout(
+                 sidebarPanel(
+                   
+                   # Input: Select file
+                   fileInput(inputId='',
+                             label= 'Load differential expression results:',
+                             placeholder = 'results.csv'),
+                   
+                   # Radio button 1: 
+                   radioButtons(inputId = '',
+                                label = 'Choose x-axis variable:',
+                                choices = c('baseMean','log2FoldChange','lfcSE',
+                                            'stat', 'pvalue','padj'),
+                                selected = 'log2FoldChange'),
+                   
+                   # Radio button 2: 
+                   radioButtons(inputId = '',
+                                label = 'Choose y-axis variable:',
+                                choices = c('baseMean','log2FoldChange','lfcSE',
+                                            'stat', 'pvalue','padj'),
+                                selected = 'padj'),
+                   
+                   # Slider: 
+                   sliderInput(inputId = '',
+                               label = 'Select the magnitude of the p-adjusted coloring:',
+                               min = -300,
+                               max = 0,
+                               value = -10),
+                   
+                   # Plot Button
+                   actionButton(inputId = "",
+                                label = "Plot",
+                                icon = icon('chart', class="fa fa-area-chart") )
+                   
+                 ),
+                 
+                 
+                 
+                 mainPanel(
+                   # Tabs
+                   tabsetPanel(
+                     # Tab 1: Samples
+                     tabPanel("1",
+                     ),
+                     # Tab 2: Counts
+                     tabPanel("2",
+                     ),
+                   )
+                 ),
+                 
+                 
+               )
       ),
       
-#---------------------------Tab 2: Counts------------------------------------
+      #---------------------------Tab 2: Counts------------------------------------
       # Tab 2: Counts
       tabPanel("Counts",
+               
+               # Sidebar layout
+               sidebarLayout(
+                 sidebarPanel(
+                   
+                   # Input: Select file
+                   fileInput(inputId='',
+                             label= 'Load differential expression results:',
+                             placeholder = 'results.csv'),
+                   
+                   # Radio button 1: 
+                   radioButtons(inputId = '',
+                                label = 'Choose x-axis variable:',
+                                choices = c('baseMean','log2FoldChange','lfcSE',
+                                            'stat', 'pvalue','padj'),
+                                selected = 'log2FoldChange'),
+                   
+                   # Radio button 2: 
+                   radioButtons(inputId = '',
+                                label = 'Choose y-axis variable:',
+                                choices = c('baseMean','log2FoldChange','lfcSE',
+                                            'stat', 'pvalue','padj'),
+                                selected = 'padj'),
+                   
+                   # Slider: 
+                   sliderInput(inputId = '',
+                               label = 'Select the magnitude of the p-adjusted coloring:',
+                               min = -300,
+                               max = 0,
+                               value = -10),
+                   
+                   # Plot Button
+                   actionButton(inputId = "",
+                                label = "Plot",
+                                icon = icon('chart', class="fa fa-area-chart") )
+                   
+                 ),
+                 
+                 
+                 
+                 mainPanel(
+                   # Tabs
+                   tabsetPanel(
+                     # Tab 1: Samples
+                     tabPanel("1",
+                     ),
+                     # Tab 2: Counts
+                     tabPanel("2",
+                     ),
+                   )
+                 ),
+                 
+                 
+               )
+               
+               
       ),
       
-#---------------------------Tab 3: Differential Expression-------------------
+      #---------------------------Tab 3: Differential Expression-------------------
       # Tab 3: Differential Expression
       tabPanel("DE",
                
@@ -33,7 +143,7 @@ ui <- fluidPage(
                  sidebarPanel(
                    
                    # Input: Select file
-                   fileInput(inputId='fileupload',
+                   fileInput(inputId='fileuploadt3',
                              label= 'Load differential expression results:',
                              placeholder = 'results.csv'),
                    
@@ -98,10 +208,66 @@ ui <- fluidPage(
                  
                )
       ),
-
-#---------------------------Tab 4: Gene Set Enrichment Analysis-------------------
+      
+      #---------------------------Tab 4: Gene Set Enrichment Analysis-------------------
       # Tab 4: Gene Set Enrichment Analysis
       tabPanel("GSEA",
+               
+               
+               # Sidebar layout
+               sidebarLayout(
+                 sidebarPanel(
+                   
+                   # Input: Select file
+                   fileInput(inputId='',
+                             label= 'Load differential expression results:',
+                             placeholder = 'results.csv'),
+                   
+                   # Radio button 1: 
+                   radioButtons(inputId = '',
+                                label = 'Choose x-axis variable:',
+                                choices = c('baseMean','log2FoldChange','lfcSE',
+                                            'stat', 'pvalue','padj'),
+                                selected = 'log2FoldChange'),
+                   
+                   # Radio button 2: 
+                   radioButtons(inputId = '',
+                                label = 'Choose y-axis variable:',
+                                choices = c('baseMean','log2FoldChange','lfcSE',
+                                            'stat', 'pvalue','padj'),
+                                selected = 'padj'),
+                   
+                   # Slider: 
+                   sliderInput(inputId = '',
+                               label = 'Select the magnitude of the p-adjusted coloring:',
+                               min = -300,
+                               max = 0,
+                               value = -10),
+                   
+                   # Plot Button
+                   actionButton(inputId = "",
+                                label = "Plot",
+                                icon = icon('chart', class="fa fa-area-chart") )
+                   
+                 ),
+                 
+                 
+                 
+                 mainPanel(
+                   # Tabs
+                   tabsetPanel(
+                     # Tab 1: Samples
+                     tabPanel("1",
+                     ),
+                     # Tab 2: Counts
+                     tabPanel("2",
+                     ),
+                   )
+                 ),
+                 
+                 
+               )
+               
       ),
     )
   )
@@ -113,43 +279,26 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output, session) {
   
+  
+  
+  #---------------------------Tab 1: Samples-------------------------------------
+  
+  #---------------------------Tab 2: Counts--------------------------------------
+  
+  #---------------------------Tab 3: Differential Expression----------------------  
   #' load_Data
   #'
-  #' @details Okay this one is a little weird but bear with me here. This is 
-  #' still a "function", but it will take no arguments. The `reactive({})` bit 
-  #' says "if any of my inputs (as in, input$...) are changed, run me again". 
-  #' This is useful when a user clicks a new button or loads a new file. In 
-  #' our case, look for the uploaded file's datapath argument and load it with 
-  #' read.csv. Return this data frame in the normal return() style.
-  load_data <- reactive({
-    DE_results <- read.csv(input$fileupload$datapath) %>%
+  #' @details Ok
+  load_data_t3 <- reactive({
+    DE_results <- read.csv(input$fileuploadt3$datapath) %>%
       rename(gene = 1)
     return(DE_results)
   })
   
-#---------------------------Tab 1: Samples-------------------------------------
   
-#---------------------------Tab 2: Counts--------------------------------------
-  
-#---------------------------Tab 3: Differential Expression----------------------  
   #' Volcano plot
   #'
-  #' @param dataf The loaded data frame.
-  #' @param x_name The column name to plot on the x-axis
-  #' @param y_name The column name to plot on the y-axis
-  #' @param slider A negative integer value representing the magnitude of
-  #' p-adjusted values to color. Most of our data will be between -1 and -300.
-  #' @param color1 One of the colors for the points.
-  #' @param color2 The other colors for the points. Hexadecimal strings: "#CDC4B5"
-  #'
-  #' @return A ggplot object of a volcano plot
-  #' @details I bet you're tired of these plots by now. Me too, don't worry.
-  #' This is _just_ a normal function. No reactivity, no bells, no whistles. 
-  #' Write a normal volcano plot using geom_point, and integrate all the above 
-  #' values into it as shown in the example app. The testing script will treat 
-  #' this as a normal function.
-  #' 
-  #' !!sym() may be required to access column names in ggplot aes().
+  #' @details I
   #'
   #' @examples volcano_plot(df, "log2fc", "padj", -100, "blue", "taupe")
   volcano_plot <-
@@ -168,19 +317,7 @@ server <- function(input, output, session) {
     }
   
   #' Draw and filter table
-  #'
-  #' @param dataf Data frame loaded by load_data()
-  #' @param slider Negative number, typically from the slider input.
-  #'
-  #' @return Data frame filtered to p-adjusted values that are less than 
-  #' 1 * 10^slider, columns for p-value and p-adjusted value have more digits 
-  #' displayed.
-  #' @details Same as above, this function is a standard R function. Tests will 
-  #' evaluate it normally. Not only does this function filter the data frame to 
-  #' rows that are above the slider magnitude, it should also change the format 
-  #' of the p-value columns to display more digits. This is so that it looks 
-  #' better when displayed on the web page. I would suggest the function 
-  #' `formatC()`
+  #' @details Same
   #'
   #' @examples draw_table(deseq_df, -210)
   #'    X  baseMean     log2FC     lfcSE      stat       pvalue         padj
@@ -196,18 +333,18 @@ server <- function(input, output, session) {
   #' These outputs aren't really functions, so they don't get a full skeleton, 
   #' but use the renderPlot() and renderTabel() functions to return() a plot 
   #' or table object, and those will be displayed in your application.
-  output$volcano <- renderPlot({req(input$fileupload)
-    volcano_plot(load_data(), input$xaxis, input$yaxis, input$sliderp, input$basecolor, input$threshcolor)
+  output$volcano <- renderPlot({req(input$fileuploadt3)
+    volcano_plot(load_data_t3(), input$xaxis, input$yaxis, input$sliderp, input$basecolor, input$threshcolor)
   })
   
   # Same here, just return the table as you want to see it in the web page
-  output$table <- renderTable({req(input$fileupload)
-    draw_table(load_data(), input$sliderp)
+  output$table <- renderTable({req(input$fileuploadt3)
+    draw_table(load_data_t3(), input$sliderp)
   })
   
   
   
-#---------------------------Tab 4: Differential Expression----------------------
+  #---------------------------Tab 4: Differential Expression----------------------
 }
 
 
